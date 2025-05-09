881afdbb34b142e77d1627e413d56429a4f6c1e0
hgjtu <vlrtmsq@gmail.com>
2025-05-09 13:54:57 +0300

    да ну все уже

---
+++ backend/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="foodgram",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "Django>=3.2,<4.0",
+        "djangorestframework>=3.12.0",
+        "psycopg2-binary>=2.9.0",
+        "Pillow>=8.0.0",
+    ],
+) 
