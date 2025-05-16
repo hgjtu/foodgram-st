ebd49489d0214d3c3f00da88d6072f81d463fb66
hgjtu <vlrtmsq@gmail.com>
2025-05-17 01:06:25 +0300

    короткие ссылки

--- backend/foodgramApi/api/views/shortlinks.py
+++ backend/foodgramApi/api/views/shortlinks.py
@@ -4,16 +4,18 @@ from recipes.models import Recipe
 from ..serializers.recipes import RecipeListSerializer
 from django.shortcuts import get_object_or_404
 from django.http import Http404
-from rest_framework.decorators import action
+from rest_framework.response import Response
 
 
 class ShortLinkRecipeViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeListSerializer
     permission_classes = [AllowAny]
+    lookup_field = 'pk'
 
-    @action(detail=True, methods=['get'], permission_classes=[AllowAny])
-    def get_object(self, short_hash=None):
+    def get_object(self):
+        short_hash = self.kwargs.get('pk')
+        
         if not short_hash:
             raise Http404("Short hash not found in URL.")
 
@@ -27,3 +29,8 @@ class ShortLinkRecipeViewSet(viewsets.ReadOnlyModelViewSet):
 
         self.check_object_permissions(self.request, obj)
         return obj
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return Response(serializer.data)
